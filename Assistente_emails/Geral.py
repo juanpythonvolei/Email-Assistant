6f9a4b79887b09c5fc95fc6a451c4e765e7c1893
juanpythonvolei <juanzsalca@outlook.com>
2024-09-15 12:53:36 -0300

    Update Geral.py

--- Assistente_emails/Geral.py
+++ Assistente_emails/Geral.py
@@ -11,13 +11,13 @@ if usuario:
         data = st.date_input(value=None,label='Insira uma data')
         if data:
             tab1,tab2 = st.tabs(['Email caixa de entrada','Não lidos'])
-            with tab1:
+            if tab1:
                 dia = str(data).split('-')
                 for info in carregar_emails(email = usuario,password = st.secrets["EMAIl"],host='imap.gmail.com',data_informar=data):
                     if int(info['data'][4:7]) == int(dia[2]):
                         st.info(info['texto'])
                         st.divider()
-            with tab2:
+            if tab2:
                     dia = str(data).split('-')
                     for info in carregar_emails_nao_lidos(email = usuario,password = st.secrets["EMAIl"],host='imap.gmail.com',data_informar=data):
                         if int(info['data'][4:7]) == int(dia[2]):
