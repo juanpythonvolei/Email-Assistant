5ee9423e5d177cb78f90a7f89491e6e0fbda7e53
juanpythonvolei <juanzsalca@outlook.com>
2024-09-15 16:08:02 -0300

    Update emails_gmail.py

--- Assistente_emails/emails_gmail.py
+++ Assistente_emails/emails_gmail.py
@@ -130,7 +130,7 @@ def baixar_emails(email):
                 nome_arquivo = anexo.filename
                 conteudo_arquivo = anexo.payload
                 tipo = anexo.content_type
-                lista_anexos.append({'conteudo':conteudo_arquivo,"nome":nome_arquivo,"remetente":email.sent_from,"assunto":email.subject})
+                lista_anexos.append({'conteudo':conteudo_arquivo,"nome":nome_arquivo})
         download = st.download_button(label="Faça o download do anexo aqui",data=lista_anexos[0]['conteudo'] in lista_anexos,file_name=f"{nome_arquivo}",mime=f"{tipo}")                                                                                            
         if download:
                                 st.success('Arquivo excel baixado com sucesso')
