bcde46c68870bc9ff8f271a7c2030d246499d6d5
juanpythonvolei <juanzsalca@outlook.com>
2024-09-16 07:12:14 -0300

    Update login.py

--- Assistente_emails/login.py
+++ Assistente_emails/login.py
@@ -1,6 +1,10 @@
 import streamlit as st 
 from emails_gmail import carregar_emails,detalhes,carregar_emails_nao_lidos
 from analise_emails import analisar_email
+import firebase_admin
+from firebase_admin import credentials, firestore,db
+import requests
+import os
 
 st.markdown("""
     <style>
@@ -9,6 +13,26 @@ st.markdown("""
         }
     </style>
 """, unsafe_allow_html=True)
+if not firebase_admin._apps:
+    autenticacao  = {
+            "type": st.secrets["firebase"]["type"],
+            "project_id": st.secrets["firebase"]["project_id"],
+            "private_key_id": st.secrets["firebase"]["private_key_id"],
+            "private_key": st.secrets["firebase"]["private_key"],
+            "client_email": st.secrets["firebase"]["client_email"],
+            "client_id": st.secrets["firebase"]["client_id"],
+            "auth_uri": st.secrets["firebase"]["auth_uri"],
+            "token_uri": st.secrets["firebase"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    }
+
+    cred = credentials.Certificate(autenticacao)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://bancodedadosroteirooficial-default-rtdb.firebaseio.com'  # Substitua pelo URL do seu Realtime Database
+    })
+else:
+    pass
 login  = st.text_input(label='',placeholder='Insira seu usuário de login')
 senha = st.text_input(label='',placeholder='Insira sua senha')
 entrar = st.button('logar')
