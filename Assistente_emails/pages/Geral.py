e9dac1d3eb17078a37c162cc8488c60ec994f2db
juanpythonvolei <juanzsalca@outlook.com>
2024-09-15 22:48:01 -0300

    Create Geral.py

---
+++ Assistente_emails/pages/Geral.py
@@ -0,0 +1,22 @@
+tab1,tab2 = st.tabs(['Email caixa de entrada','Não lidos'])
+with tab1:
+        usuario = st.text_input(label='',placeholder='Insira seu gmail',key = 'caixa de entrada')
+        if usuario:
+                data = st.date_input(value=None,label='Insira uma data',key = 'data_caixa_de_entrada')
+                if data:
+                        dia = str(data).split('-')
+                        for info in carregar_emails(email = usuario,password = st.secrets["EMAIl"],host='imap.gmail.com',data_informar=data):
+                            if int(info['data'][4:7]) == int(dia[2]):
+                                st.info(info['texto'])
+                                st.divider()
+with tab2:
+        usuario = st.text_input(label='',placeholder='Insira seu gmail',key = 'email_nao_lido')
+        if usuario:
+                data = st.date_input(value=None,label='Insira uma data',key = 'data_email_nao_lido')
+                if data:
+                    dia = str(data).split('-')
+                    for info in carregar_emails_nao_lidos(email = usuario,password = st.secrets["EMAIl"],host='imap.gmail.com',data_informar=data):
+                        if int(info['data'][4:7]) == int(dia[2]):
+                            st.info(info['texto'])
+                            st.divider()
+
