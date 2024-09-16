d213ea08ec30c20e611878e92934227d0a1d1fed
juanpythonvolei <juanzsalca@outlook.com>
2024-09-15 22:57:56 -0300

    Update login.py

--- Assistente_emails/login.py
+++ Assistente_emails/login.py
@@ -2,7 +2,13 @@ import streamlit as st
 from emails_gmail import carregar_emails,detalhes,carregar_emails_nao_lidos
 from analise_emails import analisar_email
 
-
+st.markdown("""
+    <style>
+       [aria-expanded='true'] {
+            display: none;
+        }
+    </style>
+""", unsafe_allow_html=True)
 login  = st.text_input(label='',placeholder='Insira seu usuário de login')
 senha = st.text_input(label='',placeholder='Insira sua senha')
 entrar = st.button('logar')
